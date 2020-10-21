5ea71c55ddecd5279ea0209873c118c0b2bf4a06
rkz1337 <booster2228@yandex.ru>
2020-10-21 14:28:55 +0300

    Add actions seeder

---
+++ src/migration/1603279367149-SeedActions.ts
@@ -0,0 +1,12 @@
+import { MigrationInterface, QueryRunner, getRepository } from 'typeorm';
+import { CategorySeed } from '../seeds/actions.seed';
+
+export class SeedActions1603279367149 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await getRepository('actions').save(CategorySeed);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // do nothing
+  }
+}
---
+++ src/seeds/actions.seed.ts
@@ -0,0 +1,23 @@
+export const CategorySeed = [
+  {
+    display: 'Visit page "One"',
+  },
+  {
+    display: 'Visit page "Two"',
+  },
+  {
+    display: 'Visit page "Three"',
+  },
+  {
+    display: 'Visit page "Actions"',
+  },
+  {
+    display: 'Submit form',
+  },
+  {
+    display: 'Visit page "Actions Report"',
+  },
+  {
+    display: 'Visit page "Sessions Report"',
+  },
+];
